0af4bfafb2dc24d4b10dde3211c1c1c3e89e19df
CarterR21 <ciretzlaff21@gmail.com>
2025-04-23 18:27:54 -0500

    Update Exploratory Data Analysis.sql

--- MySQL Layoffs 2022/Exploratory Data Analysis.sql
+++ MySQL Layoffs 2022/Exploratory Data Analysis.sql
@@ -49,12 +49,6 @@ FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
-SELECT SUBSTRING(`date`,1,7) AS `MOnth`, SUM(total_laid_off)
-FROM layoffs_staging2
-WHERE SUBSTRING(`date`,1,7) IS NOT NULL
-GROUP BY `MONTH`
-ORDER BY 1 ASC;
-
 WITH Rolling_Total AS
 (
 SELECT SUBSTRING(`date`,1,7) AS `MOnth`, SUM(total_laid_off) AS total_off
